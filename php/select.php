e650e3123005eb4ef720012ccce0ee9850c26230
ViniciusTaborda <vinicius.taborda.costa@gmai.com>
2021-03-18 21:50:10 -0300

    Iniciando comunicação com banco no PHP.

---
+++ php/select.php
@@ -0,0 +1,18 @@
+<?php
+    
+    include "./config.php";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT * FROM user";
+    $result = $conn->query($sql);
+
+    echo json_encode($result->fetch_assoc());
+    
+    $conn->close();
+
+    ?>
