bbd1fcdc68ad5792504547e02009eea9bd9869c1
ViniciusTaborda <vinicius.taborda.costa@gmail.com>
2021-05-27 19:56:00 -0300

    Alterado bug no arquivo de .sql

--- db/user_project_v1.sql
+++ db/user_project_v1.sql
@@ -64,8 +64,8 @@ CREATE TABLE `films` (
   `synopsis` text NOT NULL,
   `trailer` text NOT NULL,
   `image` varchar(200) NOT NULL,
-  `typeVideo` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `typeVideo` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8_general_ci;
 
 --
 -- Extraindo dados da tabela `films`
